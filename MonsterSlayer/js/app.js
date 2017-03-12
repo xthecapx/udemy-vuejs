10df53cd98d82d0a195c9109a30d240c4ae0ab26
Im Cap Marquez <Im Cap Marquez>
2017-03-12 10:58:29 -0500

    Game Monster vs Main finish

--- MonsterSlayer/js/app.js
+++ MonsterSlayer/js/app.js
@@ -5,6 +5,25 @@ new Vue({
     health: {
       main: 100,
       monster: 100
+    },
+    attacks: {
+      basic: 10,
+      special: 20
+    },
+    logs: [],
+    myClass: 'title',
+    messages: {
+      start: 'Battle Start',
+      monster: {
+        basic: 'Basic Attack, damage done to Main: ',
+        special: 'Special Attack, damage done to Main: ',
+        heals: 'Monster is healing: '
+      },
+      main: {
+        basic: 'Basic attack, damage done to Monster:  ',
+        special: 'Special Attack, damage done to Monster: ',
+        heals: 'Main is healing: '
+      }
     }
   },
   computed: {
@@ -20,12 +39,76 @@ new Vue({
     }
   },
   methods: {
-    calculateAttack: function() {
-      var mainAttack = Math.random() * 10;
-      var monsterAttack = Math.random() * 10;
+    gameLogic: function(option) {
 
-      this.health.main -= monsterAttack;
-      this.health.monster -= mainAttack;
+      // Main Logic
+      //this.logs.push("Main turn");
+      this.gameOptions(option, 'main');
+
+      //Monster Logic
+      //this.logs.push("Monster turn");
+      var monsterOption = Math.round(Math.random()) * 2;
+      this.gameOptions(monsterOption, 'monster');
+
+      this.checkWinner();
+
+    },
+    setClasses: function(main, monster, message) {
+      this.class.main = main;
+      this.class.monster = monster;
+      this.class.message = message;
+    },
+    gameOptions: function(option, character) {
+      var duel = character == 'main' ? 'monster' : 'main';
+
+      switch(option) {
+        case 0: {
+          this.characterHeals(character);
+          break;
+        }
+        case 1: {
+          this.characterAttack(duel, character, this.attacks.basic);
+          break;
+        }
+        case 2: {
+          this.characterAttack(duel, character, this.attacks.special);
+          break;
+        }
+      }
+    },
+    checkWinner: function() {
+      if ( this.health.monster <= 0) {
+        alert("Main win");
+      } else if ( this.health.main <= 0 ) {
+        alert("Monster win");
+      }
+    },
+    battleStart: function() {
+      this.showControls = true;
+      //this.logs.push(this.messages.start);
+    },
+    characterAttack: function(character, logTo, times) {
+      var attack = Math.round(Math.random()) * times + 10;
+      this.health[character] -= attack;
+
+      if (times == this.attacks.basic) {
+        this.logger(logTo, 'basic', attack);
+      } else if (times == this.attacks.special) {
+        this.logger(logTo, 'special', attack);
+      }
+      
+    },
+    characterHeals: function(character) {
+      this.health[character] += 10;
+      this.logger(character, 'heals', 10);
+    },
+    logger: function(character, type, attack) {
+        this.logs.push(this.messages[character][type] + attack);
+    },
+    giveUp: function() {
+      this.health.main = 0;
+
+      this.checkWinner();
     }
   }
 });
