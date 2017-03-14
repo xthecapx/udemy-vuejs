57b4f79c7d2e6028d421fd97910346625ce62b52
Im Cap Marquez <Im Cap Marquez>
2017-03-14 14:20:57 -0500

    Added module 11

--- Exercise Forms/src/App.vue
+++ Exercise Forms/src/App.vue
@@ -10,9 +10,46 @@
                     <!-- Password -->
                     <!-- Store Data? Yes/No -->
 
+                    <h1>Signup information</h1>
+                    <hr>
+                    <full-Name v-model="username"></full-name>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input
+                                type="text"
+                                id="email"
+                                class="form-control"
+                                v-model="userData.email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                                type="password"
+                                id="password"
+                                class="form-control"
+                                v-model="userData.password">
+                    </div>
+                    <div class="form-group checkbox">
+                        <label>
+                            <input 
+                                type="checkbox" 
+                                v-model="userData.store">
+                                Store in data base
+                        </label>
+                    </div>
+
                     <!-- Exercise 2 -->
                     <!-- Only display the Form if it has NOT been submitted -->
                     <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                            <button
+                                    class="btn btn-primary"
+                                    @click.prevent="submitted">Submit!
+                            </button>
+                        </div>
+                    </div>
 
                     <!-- Exercise 3 -->
                     <!-- Edit the Example from above and create a custom "Full Name" Control -->
@@ -21,17 +58,17 @@
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmmited">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{ username.firts }} {{ username.last }}</p>
+                        <p>Mail: {{ userData.email }} </p>
+                        <p>Password: {{ userData.password }}</p>
+                        <p>Store in Database?: {{ userData.store }}</p>
                     </div>
                 </div>
             </div>
@@ -40,7 +77,32 @@
 </template>
 
 <script>
+    import FullName from './FullName.vue'
     export default {
+        data() {
+            return {
+                userData: {
+                    name: 'Cristian',
+                    lastname: 'Marquez',
+                    password: 'password',
+                    email: 'email@email.com',
+                    store: true,
+                    username: {
+                        firts: '',
+                        last: ''
+                    }
+                },
+                isSubmmited: false
+            }
+        },
+        methods: {
+            submitted() {
+                this.isSubmmited = true
+            }
+        },
+        components: {
+            FullName
+        }
     }
 </script>
 
---
+++ Exercise Forms/src/FullName.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+    <div class="form-group">
+        <label for="name">First Name</label>
+        <input
+                type="text"
+                id="name"
+                class="form-control"
+                @input="firstnameChange"
+                :value="username.firts">
+    </div>
+    <div class="form-group">
+        <label for="lastname">Last Name</label>
+        <input
+                type="text"
+                id="lastname"
+                class="form-control"
+                @input="lastnameChange"
+                :value="username.last">
+    </div>
+</div>
+</template>
+
+<script>
+  export default {
+    props: {
+      username: {
+        type: Object,
+        default() {
+          return {
+            "firts": "default",
+            "last": "default"
+          }
+        }
+      }
+    },
+    methods: {
+      firstnameChange(event) {
+        this.username['firts'] = event.target.value;
+        this.$emit("input", this.username)
+      },
+      lastnameChange(event) {
+        this.username['last'] = event.target.value;
+        this.$emit("input", this.username)
+      }
+    }
+  }
+  
+</script>
