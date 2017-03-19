097a96f985961a98796e4674f2b9a647bd53f3ef
Im Cap Marquez <Im Cap Marquez>
2017-03-19 11:20:28 -0500

    Added animation

--- Super Quiz/src/App.vue
+++ Super Quiz/src/App.vue
@@ -8,9 +8,12 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-            
+                <transition
+                    enter-active-class="animated flipInY"
+                    leave-active-class="animated flipOutY"
+                    mode="out-in">
                     <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
-        
+                </transition>
             </div>
         </div>
     </div>
